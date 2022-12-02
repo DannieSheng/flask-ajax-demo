0a949c79d06b5205ac13f8671fa5ea59f1e66661
DannieSheng <hudanyun.sheng@gmail.com>
2022-12-02 10:54:24 -0600

    update with ajax

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
 
 
 import math
@@ -6,31 +6,46 @@ import math
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-  QTc_result = False
-  if request.method == 'POST':
-    form = request.form
-    QTc_result = calculate_qtc(form)
-  return render_template('index.html', QTc_result=QTc_result)
-
-
-def calculate_qtc(form):
-  sex = request.form['sex']
-  heart_rate = int(request.form['hr'])
-  qt_int = int(request.form['qt'])
+  return render_template('index.html')
+@app.route('/process_qtc', methods=['POST', 'GET'])
+def process_qt_calculation():
+  if request.method == "POST":
+    qtc_data = request.get_json()
+    print(qtc_data)
  
-  qt_seconds = qt_int / 1000 
-  rr_interval = (6000 / heart_rate) 
-  QTc = qt_seconds / math.sqrt(rr_interval) 
-  formated_QTc = round((QTc * 1000) * 10, 0)
-  if (formated_QTc > 440 and sex == 'm') or (formated_QTc > 460 and sex == 'f'):
-    prolonged = True
-  else:
-    prolonged = False
-  return (formated_QTc, prolonged)
+  results = {'processed': 'true'}
+  return jsonify(results)
 
+
+##############################
+######## without ajax ########
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#   QTc_result = False
+#   if request.method == 'POST':
+#     form = request.form
+#     QTc_result = calculate_qtc(form)
+#   return render_template('index.html', QTc_result=QTc_result)
+
+
+# def calculate_qtc(form):
+#   sex = request.form['sex']
+#   heart_rate = int(request.form['hr'])
+#   qt_int = int(request.form['qt'])
+ 
+#   qt_seconds = qt_int / 1000 
+#   rr_interval = (6000 / heart_rate) 
+#   QTc = qt_seconds / math.sqrt(rr_interval) 
+#   formated_QTc = round((QTc * 1000) * 10, 0)
+#   if (formated_QTc > 440 and sex == 'm') or (formated_QTc > 460 and sex == 'f'):
+#     prolonged = True
+#   else:
+#     prolonged = False
+#   return (formated_QTc, prolonged)
+######## without ajax ########
+##############################
 
 if __name__ == "__main__":
   app.run(debug=True)
